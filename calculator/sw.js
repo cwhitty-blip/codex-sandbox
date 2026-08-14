428134596729a784c39c203e23a0224aed63bbc7
cwhitty-blip <cwhitson@gmail.com>
2026-08-14 11:17:49 -0500

    Refresh Calculator offline cache

--- calculator/sw.js
+++ calculator/sw.js
@@ -1,5 +1,5 @@
-const CACHE='calculator-v3';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./upgrade.js'];
+const CACHE='calculator-v4';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./app.css','./upgrade.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 async function injectUpgrade(resp){
