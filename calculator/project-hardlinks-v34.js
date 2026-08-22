9b50dbad244d8f0a69114a06d348bb3c9a9d9526
cwhitty-blip <cwhitson@gmail.com>
2026-08-22 11:17:18 -0500

    Hard-wire Ninja project links

---
+++ calculator/project-hardlinks-v34.js
@@ -0,0 +1,10 @@
+(()=>{
+const fixed={
+  ninja:'https://ninja-y-game.cwhit.chatgpt.site/',
+  ninjay:'https://ninja-y-game.cwhit.chatgpt.site/',
+  mowing:'https://malachis-mowing-fort-scott.cwhit.chatgpt.site',
+  brainrot:'https://brainrot-movie-maker.cwhit.chatgpt.site'
+};
+for(const [id,url] of Object.entries(fixed)) localStorage.setItem('project-url-'+id,url);
+window.CalculatorProjectLinks=Object.freeze({...fixed});
+})();
